362759f6649c58cb6d7dd91cd7214ee3f2c1c186
ItayElf <thh123321@gmail.com>
2023-08-04 14:25:53 +0300

    Feat: added css renderer

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 """
 The main file
 """
+import pathlib
 from renderer.context import Context
+from renderer.head_renderer import render_head
+
+OUTPUT_FILE = pathlib.Path(__file__).parent / \
+    pathlib.Path("build", "output.html")
+
+
+def save_output(content: str):
+    """
+    Saves the output to the output file
+    """
+    OUTPUT_FILE.parent.mkdir(parents=True, exist_ok=True)
+    OUTPUT_FILE.touch(exist_ok=True)
+    OUTPUT_FILE.write_text(content)
 
 
 def main() -> None:
@@ -10,7 +24,11 @@ def main() -> None:
     """
     context = Context()
 
+    with context.tag("html"):
+        render_head(context)
+
     print(context.getvalue())
+    save_output(context.getvalue())
 
 
 if __name__ == "__main__":
---
+++ renderer/head_renderer.py
@@ -0,0 +1,30 @@
+"""
+A file that renders the head of the html
+"""
+import pathlib
+from renderer.context import Context
+
+FILE_LOCATION = pathlib.Path(__file__).parent
+BASE_CSS = pathlib.Path(
+    FILE_LOCATION,
+    "..",
+    "static",
+    "css",
+    "base.css"
+).read_text(encoding="utf-8")
+
+
+def add_styles(context: Context):
+    """
+    Adds css to the context
+    """
+    with context.tag("style"):
+        context.text(BASE_CSS)
+
+
+def render_head(context: Context):
+    """
+    Renders the head tag
+    """
+    with context.tag("head"):
+        add_styles(context)
