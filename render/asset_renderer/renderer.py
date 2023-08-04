7bf67eda92fb2c8d44636a368c4329433b37f273
ItayElf <thh123321@gmail.com>
2023-08-04 17:08:36 +0300

    Feat: added requirements

--- render/asset_renderer/renderer.py
+++ render/asset_renderer/renderer.py
@@ -3,7 +3,7 @@ A file for rendering an asset
 """
 
 from data_structures.asset import Asset
-from render.asset_renderer.top_asset_renderer import render_top_area
+from render.asset_renderer.asset_top_renderer import render_top_area
 from render.context import Context
 
 
