a17e50e150ff4a891bb671b77e2bcfaea4a38fdf
ItayElf <thh123321@gmail.com>
2024-03-22 17:45:34 +0200

    Fix: fixed requirements.txt and setup.py missing dependencies

--- setup.py
+++ setup.py
@@ -6,17 +6,26 @@ from setuptools import setup, find_packages
 
 setup(
     name="assetBuilder",
-    version='0.1.1',
+    version="0.1.1",
     description="Command line tools for generating ironsworn assets",
-    author='Itay Ben Haim',
-    packages=find_packages(include=['asset_builder', 'asset_builder.*']),
+    author="Itay Ben Haim",
+    packages=find_packages(include=["asset_builder", "asset_builder.*"]),
     install_requires=[
+        "certifi==2024.2.2",
+        "chardet==5.2.0",
+        "charset-normalizer==3.3.2",
         "click==8.1.6",
         "colorama==0.4.6",
-        "html2image==2.0.3",
-        "reportlab==4.0.4",
+        "html2image==2.0.4.3",
+        "idna==3.6",
         "Markdown==3.4.4",
-        "yattag==1.15.1"
+        "pillow==10.2.0",
+        "reportlab==4.1.0",
+        "requests==2.31.0",
+        "urllib3==2.2.1",
+        "watchdog==4.0.0",
+        "websocket-client==1.7.0",
+        "yattag==1.15.1",
     ],
     entry_points={
         "console_scripts": ["assetBuilder=asset_builder.main:cli"]
